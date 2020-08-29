57f6e4b2a3420414656689dec26db4d4b73b5ad8
Noah Saldana <saldanan@wustl.edu>
2020-08-29 17:42:34 -0400

    Added module, added breakpoint support
    -Added createMuiTheme functionality
    -Breakpoints on text and movement added
    -Moved text to the left side

--- src/Pictures.js
+++ src/Pictures.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import './Pictures.css';
 import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core'
 
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 
-const useStyles = makeStyles((theme) => ({
-  gridList: {
-    height: 750,
-    width: 1000
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      mobile: 200,
+      xs: 300,
+      sm: 700,
+      md: 1000,
+      lg: 1400,
+      xl: 1920,
+    },
   },
+})
+const useStyles = makeStyles((themes) => ({
   text:{
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
     fontFamily: "sans-serif",
-
+    [theme.breakpoints.down('xs')]: {
+      width: "40%",
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: "60%",
+    },
+    [theme.breakpoints.up('md')]: {
+      width: "75%",
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: "25%",
+    },
   },
   root:{
     display: 'flex',
@@ -25,17 +41,18 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'hidden',
     paddingBottom: 100,
     paddingTop: 200,
-    width: "100%",
     backgroundColor: "#e8f1f5",
   },
-  blue:{
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    paddingBottom: 100,
-    paddingTop: 200,
-    backgroundColor: "red"
+  gridList:{
+    height: "750px",
+    alignSelf: "center",
+    width: "1000px",
+    [theme.breakpoints.down('xs')]: {
+      width: "200px",
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: "750px",
+    },
   }
 }));
 export default function Pictures(props) {
@@ -43,16 +60,19 @@ export default function Pictures(props) {
   
     return(
       <div className={classes.root}>
-        <div>
-          <h1><a href={props.externallink} target="_blank"rel="noopener noreferrer">{props.name}</a></h1>
+        
+        <div className={classes.text}>
+          <h1 >{props.name}</h1>
+            {props.description}
+            {/* <a href={props.externallink} target="_blank"rel="noopener noreferrer">Click here</a> */}
         </div>
-        <GridList cellHeight={700}className={classes.gridList} cols={1}>
+        <GridList cellHeight={650}className={classes.gridList} cols={1}>
           {props.link.map((tile) => (
             <GridListTile key={tile} cols={tile.cols || 1}>
                   <img src={tile} alt={tile}/>
             </GridListTile>
+            
           ))}
-          <div className={classes.text}>{props.description}</div>
         </GridList>
       </div>
     )
